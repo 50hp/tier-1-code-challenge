8e96a05e41e0d965f256b59652410e363f9a7a03
Isaac Faulkner <isaacfaulkner1@gmail.com>
2023-05-02 20:28:39 -0500

    Fixed they pushing of values into the array.

--- scripts/client.js
+++ scripts/client.js
@@ -10,16 +10,12 @@ console.log('Step 0 complete! client.js loaded!');
 // Write your code here!
 
 // 1. Start with the number 29 and set that equal to a variable named ‘highScore’;
-console.log('1.')
 
 let highScore = 29;
-console.log(highScore);
 
 // 2. Make a new empty array called myScrabbleTiles.
-console.log('2.')
 
 let myScrabbleTiles = [];
-console.log(myScrabbleTiles);
 
 // 3. Put these 6 tile objects into myScrabbleTiles:
 /*
@@ -30,35 +26,27 @@ console.log(myScrabbleTiles);
   { tile: 'D', score: 2 }
   { tile: 'A', score: 1 }
 */
-console.log('3.');
 
-myScrabbleTiles.push( [{ tile: 'N', score: 1 },
+myScrabbleTiles.push( { tile: 'N', score: 1 },
                    { tile: 'K', score: 5 },
                    { tile: 'Z', score: 10 },
                    { tile: 'X', score: 8 },
                    { tile: 'D', score: 2 },
-                   { tile: 'A', score: 1 } ] ) ;
-console.log(myScrabbleTiles);
-
+                   { tile: 'A', score: 1 }  ) ;
 // 4. Remove the last tile from myScrabbleTiles.
-console.log('4.');
 
 myScrabbleTiles.pop();
-console.log(myScrabbleTiles);
 
 
 // 5. Add the following new tile to myScrabbleTiles:
 /*
   { tile: 'F', score : 4 }
 */
-console.log('5.');
 
 myScrabbleTiles.push( { tile: 'F', score : 4 } );
-console.log(myScrabbleTiles);
 
 
 // 6. Complete this function. It needs to be given an array of tile objects. The function will use a for-loop and return the sum of all the scores in the given array.
-console.log('6.');
 
 function sumTiles( anArray ) {
   console.log( 'in sumTiles' );
@@ -76,19 +64,13 @@ function sumTiles( anArray ) {
 console.log( sumTiles( myScrabbleTiles ) );
 
 // 7. Use the function above to get the total score for myScrabbleTiles and assign the value to a variable called "myScore".
-console.log('7.');
 
 let myScore = sumTiles( myScrabbleTiles );
-console.log( myScore );
-
-
 
 // 8. Check whether or not your score is higher than the highScore.
-console.log('8.');
 
 if ( myScore > highScore ){
     highScore = myScore;
-    console.log( highScore );
 
     console.log('I beat the High Score!!');
 
